72975b269f2a9d872f90e6c6101d42767a25742c
alinkaskakovets34 <skakovets_ak24@nuwm.edu.ua>
2025-10-23 16:24:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,88 +1,86 @@
 using System;
 
-
 class MaterialPoint
 {
-    public int x0, y0, z0;
-    public int v1, v2, v3;
+    public int x0, y0, z0; // Початкові координати
+    public int v1, v2, v3; // Компоненти швидкості
 
-
-
-
+    // Метод для задання початкових координат
     public void SetData(int x, int y, int z)
     {
         x0 = x;
         y0 = y;
         z0 = z;
     }
-    // метод для задання або обрахунку через певний t із v
-    public int abracadabra(int v, int t)
+
+    // Метод для задання швидкості
+    public void SetVelocity(int vx, int vy, int vz)
     {
-        int x = x0 + v * t;
-        int y = y0 + v * t;
-        int z = z0 + v * t;
-        Console.WriteLine("new coordinates {0}, {1}, {2}", x, y, z);
-        int moroz;
-        if (x > 0 && y > 0 && z > 0)
-        {
-            moroz = 1;
-        }
-        else
-        {
-            moroz = 2;
-        }
-        if (x < 0 && y < 0 && z < 0)
-        {
-            moroz = 3;
-
-
-        }
-        else
-        {
-            moroz = 4;
-        }
-        return moroz;
-
-
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            MaterialPoint[] k = new MaterialPoint[10];
-            int x = 0, y = 0, z = 0;
-            int kx, ky, kz;
-            int count;
-            int a = 0;
-            Console.WriteLine("Enter count of points");
-            count = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < count; i++)
-            {
-                k[i] = new MaterialPoint();
-                Console.WriteLine("Enter coordinates of point {0}", i + 1);
-                Console.Write("x=");
-                kx = Convert.ToInt32(Console.ReadLine());
-                Console.Write("y=");
-                ky = Convert.ToInt32(Console.ReadLine());
-                Console.Write("z=");
-                kz = Convert.ToInt32(Console.ReadLine());
-                k[i].SetData(kx, ky, kz);
-            }
-            for (int i = 0; i < count; i++)
-            {
-                if (k[i].abracadabra(2, 3) == 1)
-                {
-                    a++;
-                    Console.WriteLine("Point {0} is in 1st octant", i + 1);
-                }
-
-
-            }
-            Console.WriteLine("Count of points in 1st octant is {0}", a);
-        }
-
-
+        v1 = vx;
+        v2 = vy;
+        v3 = vz;
     }
 
+    // Метод для обчислення нових координат і перевірки октанта
+    public bool IsInFirstOctant(int t)
+    {
+        int x = x0 + v1 * t;
+        int y = y0 + v2 * t;
+        int z = z0 + v3 * t;
 
+        Console.WriteLine($"New coordinates: ({x}, {y}, {z})");
+
+        // Перевірка, чи потрапляє точка у перший октант
+        return (x > 0 && y > 0 && z > 0);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter count of points: ");
+        int count = Convert.ToInt32(Console.ReadLine());
+
+        MaterialPoint[] points = new MaterialPoint[count];
+        int time = 0;
+
+        Console.Write("Enter time t: ");
+        time = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < count; i++)
+        {
+            points[i] = new MaterialPoint();
+
+            Console.WriteLine($"\nEnter coordinates of point {i + 1}:");
+            Console.Write("x = ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("y = ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.Write("z = ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            points[i].SetData(x, y, z);
+
+            Console.WriteLine($"Enter velocity components of point {i + 1}:");
+            Console.Write("v1 = ");
+            int v1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("v2 = ");
+            int v2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("v3 = ");
+            int v3 = Convert.ToInt32(Console.ReadLine());
+            points[i].SetVelocity(v1, v2, v3);
+        }
+
+        int countInFirstOctant = 0;
+        for (int i = 0; i < count; i++)
+        {
+            if (points[i].IsInFirstOctant(time))
+            {
+                countInFirstOctant++;
+                Console.WriteLine($"Point {i + 1} is in the 1st octant.");
+            }
+        }
+
+        Console.WriteLine($"\nCount of points in 1st octant: {countInFirstOctant}");
+    }
 }
